a74b13005737148483e41326f1b90735168e0b10
Emma SATTLER <esattler@cfai-formation.fr>
2023-10-28 13:39:19 +0200

    fix(Build)

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Set the Node.js version
+nvm use 16.13.0
+
 # Install dependencies
 npm install
 
-# Install the Prisma CLI globally with Node.js 16.13
-nvm use 16.13.0
+# Install the Prisma CLI globally
 npm install -g prisma
 
 # Generate the Prisma client library
